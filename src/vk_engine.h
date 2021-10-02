bd71fc4e147ba8dcff4bce88f444aa2653f3fac7
AbrarZShahriar <a.zshahriar@gmail.com>
2021-10-02 16:59:21 +0900

    Add project files.

---
+++ src/vk_engine.h
@@ -0,0 +1,126 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vector>
+#include <vk_types.h>
+
+#include <functional>
+
+#include <deque>
+
+struct DeletionQueue {
+  std::deque<std::function<void()>> deletors;
+
+  void push_function(std::function<void()> &&function) {
+    deletors.push_back(function);
+  }
+
+  void flush() {
+    // reverse iterate the deletion queue to execute all the functions
+    for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+      (*it)(); // call the function
+    }
+
+    deletors.clear();
+  }
+};
+
+class VulkanEngine {
+public:
+  // --- omitted ---
+  VkInstance _instance;                      // Vulkan library handle
+  VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+  VkPhysicalDevice _chosenGPU;               // GPU chosen as the default device
+  VkDevice _device;                          // Vulkan device for commands
+  VkSurfaceKHR _surface;                     // Vulkan window surface
+                                             // --- other code ---
+  VkSwapchainKHR _swapchain;                 // from other articles
+
+  // image format expected by the windowing system
+  VkFormat _swapchainImageFormat;
+
+  // array of images from the swapchain
+  std::vector<VkImage> _swapchainImages;
+
+  // array of image-views from the swapchain
+  std::vector<VkImageView> _swapchainImageViews;
+
+  // --- other code ---
+  VkQueue _graphicsQueue;        // queue we will submit to
+  uint32_t _graphicsQueueFamily; // family of that queue
+
+  VkCommandPool _commandPool;         // the command pool for our commands
+  VkCommandBuffer _mainCommandBuffer; // the buffer we will record into
+
+  //--- other code ---
+  VkRenderPass _renderPass;
+
+  std::vector<VkFramebuffer> _framebuffers;
+
+  //--- other code ---
+  VkSemaphore _presentSemaphore, _renderSemaphore;
+  VkFence _renderFence;
+
+  // loads a shader module from a spir-v file. Returns false if it errors
+  bool load_shader_module(const char *filePath,
+                          VkShaderModule *outShaderModule);
+
+  bool _isInitialized{false};
+  int _frameNumber{0};
+  int _selectedShader{0};
+
+  VkExtent2D _windowExtent{1700, 900};
+
+  struct SDL_Window *_window{nullptr};
+
+  // initializes everything in the engine
+  void init();
+
+  // shuts down the engine
+  void cleanup();
+
+  // draw loop
+  void draw();
+
+  // run main loop
+  void run();
+
+  // ... other stuff ....
+
+  VkPipelineLayout _trianglePipelineLayout;
+  // ... other objects
+  VkPipeline _trianglePipeline;
+  VkPipeline _redTrianglePipeline;
+  DeletionQueue _mainDeletionQueue;
+
+private:
+  void init_vulkan();
+  // --- other code ---
+  void init_swapchain();
+  //----- other code----
+  void init_commands();
+  //--- other code ---
+  void init_default_renderpass();
+
+  void init_framebuffers();
+  //--- other code ---
+  void init_sync_structures();
+  void init_pipelines();
+};
+
+class PipelineBuilder {
+public:
+  std::vector<VkPipelineShaderStageCreateInfo> _shaderStages;
+  VkPipelineVertexInputStateCreateInfo _vertexInputInfo;
+  VkPipelineInputAssemblyStateCreateInfo _inputAssembly;
+  VkViewport _viewport;
+  VkRect2D _scissor;
+  VkPipelineRasterizationStateCreateInfo _rasterizer;
+  VkPipelineColorBlendAttachmentState _colorBlendAttachment;
+  VkPipelineMultisampleStateCreateInfo _multisampling;
+  VkPipelineLayout _pipelineLayout;
+
+  VkPipeline build_pipeline(VkDevice device, VkRenderPass pass);
+};
