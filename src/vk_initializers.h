26b0c17c13d7d9eb91ae6b2cd011b9e44f51ef98
AbrarZShahriar <a.zshahriar@gmail.com>
2021-10-03 23:36:52 +0900

    Depth buffer added.

--- src/vk_initializers.h
+++ src/vk_initializers.h
@@ -5,25 +5,38 @@
 
 #include <vk_types.h>
 
-namespace vkinit {
+namespace vkinit
+{
 
 // vulkan init code goes here
 VkCommandPoolCreateInfo
-command_pool_create_info(uint32_t queueFamilyIndex,
-                         VkCommandPoolCreateFlags flags = 0);
+command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags = 0);
 
-VkCommandBufferAllocateInfo command_buffer_allocate_info(
-    VkCommandPool pool, uint32_t count = 1,
-    VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
-VkPipelineVertexInputStateCreateInfo vertex_input_state_create_info();
+VkCommandBufferAllocateInfo
+command_buffer_allocate_info(VkCommandPool pool, uint32_t count = 1, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+VkPipelineVertexInputStateCreateInfo
+vertex_input_state_create_info();
 VkPipelineInputAssemblyStateCreateInfo
 input_assembly_create_info(VkPrimitiveTopology topology);
 VkPipelineRasterizationStateCreateInfo
 rasterization_state_create_info(VkPolygonMode polygonMode);
-VkPipelineMultisampleStateCreateInfo multisampling_state_create_info();
-VkPipelineColorBlendAttachmentState color_blend_attachment_state();
-VkPipelineLayoutCreateInfo pipeline_layout_create_info();
+VkPipelineMultisampleStateCreateInfo
+multisampling_state_create_info();
+VkPipelineColorBlendAttachmentState
+color_blend_attachment_state();
+VkPipelineLayoutCreateInfo
+pipeline_layout_create_info();
 VkPipelineShaderStageCreateInfo
-pipeline_shader_stage_create_info(VkShaderStageFlagBits stage,
-                                  VkShaderModule shaderModule);
+pipeline_shader_stage_create_info(VkShaderStageFlagBits stage, VkShaderModule shaderModule);
+
+
+VkImageCreateInfo
+image_create_info(VkFormat format, VkImageUsageFlags usageFlags, VkExtent3D extent);
+
+VkImageViewCreateInfo
+imageview_create_info(VkFormat format, VkImage image, VkImageAspectFlags aspectFlags);
+
+VkPipelineDepthStencilStateCreateInfo
+depth_stencil_create_info(bool bDepthTest, bool bDepthWrite, VkCompareOp compareOp);
+
 } // namespace vkinit
